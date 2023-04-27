1db05a91545bc10c7f91e1e19329d9afe5252b65
VladGrigoriu <vlad.grigoriu4@gmail.com>
2023-04-27 12:16:55 +0200

    Initialize Random Walk Algorithm for procedural map generation

---
+++ Assets/Scripts/ProceduralGeneration/SimpleRandomWalkMapGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalkMapGenerator : AbstractDungeonGenerator
+{
+
+    [SerializeField]
+    private int iterations = 10;
+
+    [SerializeField]
+    public int walkLength = 10;
+
+    [SerializeField]
+    public bool startRandomlyEachIteration = true;
+
+    protected override void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk(iterations, walkLength, startRandomlyEachIteration);
+        tilemapVisualizer.Clear();
+        tilemapVisualizer.PaintFloorTiles(floorPositions);
+        WallGenerator.CreateWalls(floorPositions, tilemapVisualizer);
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk(int iterationsNumber, int walkerLength, bool startRandom)
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+
+        for (int i = 0; i < iterationsNumber; i++)
+        {
+            var path = ProceduralGenerationAlgorithms.SimpleRandomWalk(currentPosition, walkerLength);
+            floorPositions.UnionWith(path);
+            if(startRandom) currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+        }
+
+        return floorPositions;
+    }
+
+}
---
+++ Assets/Scripts/ProceduralGeneration/TilemapVisualizer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap floorTilemap, wallTilemap;
+
+    [SerializeField]
+    private TileBase floorTile, wallTop; //in future make an array and select random element to get a random floor and use it accordingly with walls tile
+
+    public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
+    {
+        PaintTiles(floorPositions, floorTilemap, floorTile);
+    }
+
+    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile) 
+    {
+        foreach (var position in positions)
+        {
+            PaintSingleTile(tilemap, tile, position);
+        }
+    }
+
+    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position) 
+    {
+        var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+        tilemap.SetTile(tilePosition, tile);
+    }
+
+    internal void PaintSingleBasicWall(Vector2Int position)
+    {
+        PaintSingleTile(wallTilemap, wallTop, position);
+    }
+
+
+
+    public void Clear()
+    {
+        floorTilemap.ClearAllTiles();
+        wallTilemap.ClearAllTiles();
+    }
+}
